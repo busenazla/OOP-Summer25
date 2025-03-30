349f96498199b1e904a2382d238e66923b82d4d8
busenazla <buse.nazla@akademiata.edu.pl>
2025-03-30 22:03:22 +0200

    Update and rename student details to student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,8 @@
+student_details = {
+    "first_name": "Buse",
+    "last_name": "Nazla",
+    "index_number": "34373",
+    "nationality": "Turkısh",
+    "starting_date": "2025-03-30",
+    "courses": ["Mathematics", "Physics", "Computer Science"]
+}
