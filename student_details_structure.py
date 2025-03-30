ddab12295dd818de8d5abc0ece83fd710df84e39
busenazla <buse.nazla@akademiata.edu.pl>
2025-03-30 22:06:48 +0200

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -6,3 +6,9 @@ student_details = {
     "starting_date": "2025-03-30",
     "courses": ["Mathematics", "Physics", "Computer Science"]
 }
+print(student_details["first_name"])
+print(student_details["last_name"])
+print(student_details["index_number"])
+print(student_details["nationality"])
+print(student_details["starting_date"])
+print(student_details["courses"])
