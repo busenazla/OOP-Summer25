3e80f8ba7180e780f03269348447ba44ce70eb20
busenazla <buse.nazla@akademiata.edu.pl>
2025-04-09 21:00:10 +0200

    animals.py

--- animals.py
+++ animals.py
@@ -30,3 +30,27 @@ animals = [
         "skills": ["slithering", "camouflage"]
     }
 ]
+
+class Animal:
+    def __init__(self, name: str, group: str, number_of_legs: int, skills: list):
+        self.name = name
+        self.group = group
+        self.number_of_legs = number_of_legs
+        self.skills = skills
+
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Group: {self.group}")
+        print(f"Number of Legs: {self.number_of_legs}")
+        print(f"Skills: {', '.join(self.skills)}")
+        print("-" * 20)
+
+dog = Animal("Dog", "mammals", 4, ["running", "swimming"])
+eagle = Animal("Eagle", "birds", 2, ["flying", "hunting"])
+shark = Animal("Shark", "fish", 0, ["swimming", "sensing blood"])
+frog = Animal("Frog", "amphibians", 4, ["jumping", "swimming"])
+snake = Animal("Snake", "reptiles", 0, ["slithering", "camouflage"])
+
+animals = [dog, eagle, shark, frog, snake]
+for animal in animals:
+    animal.display_info()
