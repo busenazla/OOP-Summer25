d904423ad6dbc987077de859c5589c6704625a52
busenazla <buse.nazla@akademiata.edu.pl>
2025-04-09 20:58:40 +0200

    animals.py

---
+++ animals.py
@@ -0,0 +1,32 @@
+animals = [
+    {
+        "name": "Dog",
+        "group": "mammals",
+        "number_of_legs": 4,
+        "skills": ["running", "swimming"]
+    },
+    {
+        "name": "Eagle",
+        "group": "birds",
+        "number_of_legs": 2,
+        "skills": ["flying", "hunting"]
+    },
+    {
+        "name": "Shark",
+        "group": "fish",
+        "number_of_legs": 0,
+        "skills": ["swimming", "sensing blood"]
+    },
+    {
+        "name": "Frog",
+        "group": "amphibians",
+        "number_of_legs": 4,
+        "skills": ["jumping", "swimming"]
+    },
+    {
+        "name": "Snake",
+        "group": "reptiles",
+        "number_of_legs": 0,
+        "skills": ["slithering", "camouflage"]
+    }
+]
