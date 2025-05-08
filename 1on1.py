cf004296d66c6ea51130c352337456c1bd21aaa8
busenazla <buse.nazla@akademiata.edu.pl>
2025-05-08 11:08:07 +0000

    Add new file

--- 1on1.py
+++ 1on1.py
@@ -1,18 +1,4 @@
-x='buse'
-print(x)
+print("hello world")
 
-name='buse' 
-number = 19
-print(type(number))
-
-n = 24.6
-print(type(n))
-
-cars = ['bmw', 'mercedes', 'porche']
-for x in cars:
- print(x)
-
- # todo
- # create dictionary 
- # create class
- # if statement 
+x = 6
+print()
